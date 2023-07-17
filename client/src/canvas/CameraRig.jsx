18456a6f70d5585dac9c2dd53d70b5dcc659876a
shahzaib <f201079@cfd.nu.edu.pk>
2023-07-17 22:11:24 +0500

    App done

--- client/src/canvas/CameraRig.jsx
+++ client/src/canvas/CameraRig.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useRef } from "react";
+import { useFrame } from "@react-three/fiber";
+import { easing } from "maath";
+import { useSnapshot } from "valtio";
 
-const CameraRig = () => {
-  return <div>CameraRig</div>;
+import state from "../store";
+// passing children components as props
+const CameraRig = ({ children }) => {
+  // We gonna use this ref to update the state
+  const group = useRef();
+  const snap = useSnapshot(state);
+
+  // UseFrame Allows to execute code on every rendered frame
+  useFrame((state, delta) => {
+    // setting breakpoints
+    const isBreakpoint = window.innerWidth <= 1260;
+    const isMobile = window.innerWidth <= 600;
+    // set initial positions of the model for all the screen sizes
+    let targetPosition = [-0.4, 0, 2];
+    if (snap.intro) {
+      if (isBreakpoint) targetPosition = [0, 0, 2];
+      if (isMobile) targetPosition = [0, 0.2, 2.5];
+    } else {
+      if (isMobile) targetPosition = [0, 0, 2.5];
+      else targetPosition = [0, 0, 2];
+    }
+    // set modal camera position (makes shirt size a bit bigger)
+    easing.damp3(state.camera.position, targetPosition, 0.25, delta);
+    // set the model rotation smoothly (moving shirt)
+    easing.dampE(
+      group.current.rotation,
+      [state.pointer.y / 10, -state.pointer.x / 5, 0],
+      0.25,
+      delta
+    );
+  });
+
+  return <group ref={group}>{children}</group>;
 };
 
 export default CameraRig;
--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -1,10 +1,34 @@
-import React from "react";
+import { Canvas } from "@react-three/fiber";
+import { Environment, Center } from "@react-three/drei";
+
+import Shirt from "./Shirt";
+import Backdrop from "./Backdrop";
+import CameraRig from "./CameraRig";
 
 const CanvasModel = () => {
   return (
-    <div>
-      <h1>Canvas</h1>
-    </div>
+    // Everything will come inside this tag
+    // Camera properties inside the Canvas to make shirt bigger
+    <Canvas
+      shadows
+      camera={{ position: [0, 0, 0], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+      className="w-full max-w-full h-full transition-all ease-in"
+    >
+      <ambientLight intensity={0.5} />
+      {/* this will have affect on whole screen */}
+      <Environment preset="city" />
+      {/* To work properly, these components should return Threejs instead of normal divs */}
+      {/* We willpoint cameraRig close to shirt to maximize its size */}
+      {/* We are passing some children components in CameraRig */}
+      <CameraRig>
+        {/* <Backdrop /> */}
+        {/* This will place shirt in the center of the screen */}
+        <Center>
+          <Shirt />
+        </Center>
+      </CameraRig>
+    </Canvas>
   );
 };
 
