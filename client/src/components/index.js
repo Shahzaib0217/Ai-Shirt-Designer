a8089b4104073592278404532fb6672f82649476
shahzaib <f201079@cfd.nu.edu.pk>
2023-07-17 22:54:04 +0500

    Ai picker error

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,7 +1,7 @@
 import CustomButton from "./CustomButton";
-import AIPicker from "./AIPicker";
 import ColorPicker from "./ColorPicker";
 import FilePicker from "./FilePicker";
 import Tab from "./Tab";
+import AIPicker from "./AIPicker";
 
 export { CustomButton, AIPicker, ColorPicker, FilePicker, Tab };
